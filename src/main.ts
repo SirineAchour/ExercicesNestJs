f4a3d8359c24f5ec170a32c8b5402740f254caa3
Sirine Achour <47246110+SirineAchour@users.noreply.github.com>
2020-12-05 20:13:53 +0100

    morgan, helmet and cors DONE

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
+//import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe() )
+
+  const corsOptions = {
+    origin: ['http://localhost:4201', 'http://localhost:4200'], 
+    optionsSuccessStatus: 202
+  }
+
+  app.use(morgan('dev'))
+  app.enableCors(corsOptions)
+  //app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000);
-}
-bootstrap();
+  }
+  bootstrap();
--- src/my-first-module/my-first-module.module.ts
+++ src/my-first-module/my-first-module.module.ts
@@ -1,10 +1,22 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { TodoController } from './controllers/todo/todo.controller';
 import { TodoService } from './services/todo/todo.service';
 import { DiversController } from './controllers/divers/divers.controller';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { HelmetMiddleware } from '@nest-middlewares/helmet';
 
 @Module({
   controllers: [TodoController, DiversController],
   providers: [TodoService],
 })
-export class MyFirstModuleModule {}
+export class MyFirstModuleModule implements NestModule{
+  configure(consumer: MiddlewareConsumer): MiddlewareConsumer | void {
+    consumer.apply(HelmetMiddleware).forRoutes('')
+    .apply(LoggerMiddleware, 
+      (req: Request, res: Response, next: () => void) => {
+      console.log("In Logger Function");
+      //console.log("Request :", req);
+      next();
+    }).forRoutes({ path: '*', method: RequestMethod.ALL })
+  }
+}
